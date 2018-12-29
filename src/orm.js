c458b7cd7d61604ffa3b13e829b38de363b10d05
Peter Savichev (proton) <psavichev@gmail.com>
2018-12-29 19:02:57 +0100

    implemented all /filter methods

--- src/orm.js
+++ src/orm.js
@@ -25,13 +25,13 @@ module.exports = class Orm {
     this.insertAccountQuery.run(account);
 
     if(account.interests) {
-      const sql = 'INSERT INTO accounts_interest (account_id, interest) VALUES ' +
+      const sql = 'INSERT INTO account_interests (account_id, interest) VALUES ' +
         account.interests.map(interest => `(${account.id}, '${interest}')`).join(', ');
       this.db.exec(sql);
     }
 
     if(account.likes) {
-      const sql = 'INSERT INTO accounts_like (account_id, like_id, like_ts) VALUES ' +
+      const sql = 'INSERT INTO account_likes (account_id, like_id, like_ts) VALUES ' +
         account.likes.map(like => `(${account.id}, ${like.id}, ${like.ts})`).join(', ');
       this.db.exec(sql);
     }
@@ -61,7 +61,7 @@ module.exports = class Orm {
       premium_finish integer)
     `);
 
-    if (!tables.includes('accounts_like')) this.db.exec(`CREATE TABLE accounts_like
+    if (!tables.includes('account_likes')) this.db.exec(`CREATE TABLE account_likes
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
       like_id integer,
       like_ts integer,
@@ -69,7 +69,7 @@ module.exports = class Orm {
       FOREIGN KEY(account_id) REFERENCES accounts(id))
     `);
 
-    if (!tables.includes('accounts_interest')) this.db.exec(`CREATE TABLE accounts_interest
+    if (!tables.includes('account_interests')) this.db.exec(`CREATE TABLE account_interests
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
       interest text,
       account_id integer,
--- src/request_handlers/get_accounts_filter_handler.js
+++ src/request_handlers/get_accounts_filter_handler.js
@@ -31,7 +31,6 @@ const quiredFieldGt = (key, value, builder) => {
 };
 const quiredFieldNull = (key, value, builder) => {
   const field = 'accounts.' + key.slice(0, -5);
-  builder.selects.add(field);
   if (value === '1' ) builder.wheres.push(`${field} IS NULL`);
   else builder.wheres.push(`${field} IS NOT NULL`);
 };
@@ -68,6 +67,22 @@ const quiredBirthYear = (_key, value, builder) => {
   const tss = Utils.yearToTimestamps(value);
   builder.wheres.push(`${field} >= ${tss.from} AND ${field} < ${tss.to}`);
 };
+const quiredInterestsAny = (_key, value, builder) => {
+  const values = value.split(',').map(s => `'${Utils.escapeString(s)}'`).join(', ');
+  builder.wheres.push(`id IN (SELECT DISTINCT account_id FROM account_interests WHERE account_interests.interest IN (${values}))`);
+};
+const quiredInterestsContains = (_key, value, builder) => {
+  const values = value.split(',').map(s => `'${Utils.escapeString(s)}'`);
+  const sql = `id IN (SELECT account_id FROM account_interests WHERE account_interests.interest IN (${values.join(', ')})
+               GROUP BY account_id HAVING COUNT(DISTINCT account_interests.interest) == ${values.length})`;
+  builder.wheres.push(sql);
+};
+const quiredLikesContains = (_key, value, builder) => {
+  const values = value.split(',');
+  const sql = `id IN (SELECT account_id FROM account_likes WHERE account_likes.like_id IN (${values.join(', ')})
+               GROUP BY account_id HAVING COUNT(DISTINCT account_likes.like_id) == ${values.length})`;
+  builder.wheres.push(sql);
+};
 
 const quiredFieldsMapping = {
   'sex_eq': quiredFieldEq,
@@ -92,42 +107,14 @@ const quiredFieldsMapping = {
   'birth_lt': quiredFieldLt,
   'birth_gt': quiredFieldGt,
   'birth_year': quiredBirthYear,
-  // 'interests_contains': quiredFieldIgnore,
-  // 'interests_any': quiredFieldIgnore,
-  // 'likes_contains': quiredFieldIgnore,
+  'interests_contains': quiredInterestsContains,
+  'interests_any': quiredInterestsAny,
+  'likes_contains': quiredLikesContains,
   'premium_now': quiredPremiumNow,
   'premium_null': quiredPremiumNull,
   'query_id': quiredFieldIgnore,
   'limit': quiredFieldIgnore
 };
-
-
-//         case 'email_domain': return (account.email && account.email.split('@')[1] == value);
-//         case 'email_lt': return (account.email < value);
-//         case 'email_gt': return (account.email > value);
-//         case 'status_eq': return (account.status == value);
-//         case 'status_neq': return (account.status != value);
-//         case 'fname_eq': return (account.fname == value);
-//         case 'fname_any': return value.split(',').includes(account.fname);
-//         case 'fname_null': return (value == '1' ? !account.fname : account.fname);
-//         case 'sname_eq': return (account.sname == value);
-//         case 'sname_starts': return (account.sname && account.sname.startsWith(value));
-//         case 'sname_null': return (value == '1' ? !account.sname : account.sname);
-//         case 'phone_code': return (account.phone && account.phone.includes(`(${value})`));
-//         case 'phone_null': return (value == '1' ? !account.phone : account.phone);
-//         case 'country_eq': return (account.country == value);
-//         case 'country_null': return (value == '1' ? !account.country : account.country);
-//         case 'city_eq': return (account.city == value);
-//         case 'city_any': return value.split(',').includes(account.city);
-//         case 'city_null': return (value == '1' ? !account.city : account.city);
-//         case 'birth_lt': return (value - account.birth > 0);
-//         case 'birth_gt': return (value - account.birth < 0);
-//         case 'birth_year': return (value == Utils.timeToYear(account.birth));
-//         case 'interests_contains': return (account.interests && value.split(',').every(v => account.interests.includes(v)));
-//         case 'interests_any': return (account.interests && value.split(',').some(v => account.interests.includes(v)));
-//         case 'likes_contains': return (account.likes && value.split(',').every(v => account.likes.some(h => h.id == v)));
-//         case 'premium_now': return this.hasPremium(account);
-//         case 'premium_null': return (value == '1' ? !account.premium : account.premium);
 
 module.exports = class GetAccountsFilterHandler extends WebRequestHandler {
   call() {
